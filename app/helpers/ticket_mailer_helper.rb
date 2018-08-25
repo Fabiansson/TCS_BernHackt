e5bcb681b65b671acff71abd52b3ac04eeb3def2
Fabian Zbinden <fabian.zbinden@students.ffhs.ch>
2018-08-26 01:04:37 +0200

    ticket_helper_mailer

--- app/helpers/ticket_mailer_helper.rb
+++ app/helpers/ticket_mailer_helper.rb
@@ -4,10 +4,13 @@ module TicketMailerHelper
 
 
   def generateqr(id_abo,event_id)
-    @abo = id_abo.to_s
-    @event = event_id.to_s
-    @code = Digest::MD5.hexdigest @abo + @event
-    puts('CODE:' + @code)
-    @qr = RQRCode::QRCode.new(@code).to_img.resize(200,200).to_data_url
+    abo = id_abo.to_s
+    event = event_id.to_s
+    code = Digest::MD5.hexdigest abo + event
+
+    advert = Advert.find_by(season_pass_id: id_abo, game_id: id_event)
+    advert.update_all hashcode: hash
+
+    @qr = RQRCode::QRCode.new(code).to_img.resize(200,200).to_data_url
   end
 end
